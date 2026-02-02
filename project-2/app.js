e383acdc031062f71c29e3ce16e80bfae2c83ec5
vikasthakurr <vikaskumar20012001@gmail.com>
2026-02-02 22:32:12 +0530

    changes pushed

--- project-2/app.js
+++ project-2/app.js
@@ -8,3 +8,43 @@ toggleBtn.addEventListener("click", () => {
   const isLightTheme = document.body.classList.contains("light-theme");
   toggleBtn.textContent = isLightTheme ? "🌙" : "🌞";
 });
+
+//array typing custom function.....
+const typedTextSpan = document.querySelector(".typed-text");
+const textArray = ["Runner", "Ultra-Runner", "Software Engineer","Team Lead"];
+const typingDelay = 100;
+const erasingDelay = 50;
+const newTextDelay = 2000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(
+      0,
+      charIndex - 1
+    );
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } else {
+    textArrayIndex++;
+    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  // On DOM Load initiate the effect
+  if (textArray.length) setTimeout(type, newTextDelay + 250);
+});
+
